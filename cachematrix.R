55691f3e8876d6f12009f87874963df15ee229a6
pkhachane <pkhachane@gmail.com>
2015-09-27 12:31:57 +0200

    Changed code & working with example of 2 x 2 matrix

--- cachematrix.R
+++ cachematrix.R
@@ -9,33 +9,52 @@
 # 3) set the value of the mean
 # 4) get the value of the mean
 
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+
+makeCacheMatrix <- function(a = matrix()) {
+    # initialize the inverse matrix value
+    mat_inv <- NULL
+    
+    # set the value of the matrix
+    s_value <- function(b) {
+        a <<- b
+        mat_inv <<- NULL
     }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+    
+    # get the value of the matrix
+    g_value <- function() a
+    
+    # set the value of the inverse
+    set_inverse <- function(inv_input) mat_inv <<- inv_input
+    # get the value of the inverse
+    get_inverse <- function() mat_inv
+    
+    # return a list of all the above functions
+    list(set = s_value, get = g_value,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
+    
 }
+
 
 ## Write a short comment describing this function
 
-
-cachemean <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
+cacheSolve <- function(a, ...) {
+    # check if the inverse is already cached,
+    # if so, we get the inverse from the cache directly
+    ginv <- a$get_inverse()
+    if(!is.null(ginv)) {
+        message("getting cached inverse")
+        return(ginv)
     }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
+    # else, we first get the matrix
+    matrixd <- a$get()
+    # and calculate the inverse
+    ginv <- solve(matrixd, ...)
+    # next, cache the inverse of the matrix
+    a$set_inverse(ginv)
+    # and finally, return the result
+    ginv
 }
+
